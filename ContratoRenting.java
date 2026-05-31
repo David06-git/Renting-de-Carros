4a792cb2d2f37693d6c43c00f221d3f61bc40517
sofiapm0322-star <sofiapm0322@gmail.com>
2026-05-31 14:26:48 -0500

    Refactor ContratoRenting by removing unnecessary code
    Removed unused fields and methods from ContratoRenting class.

--- ContratoRenting.java
+++ ContratoRenting.java
@@ -1,32 +1,10 @@
 
 
-public class ContratoRenting {
+public class ContratoRenting 
 
     private String idContrato;
     private String cedulaCliente;
     private String placaVehiculo;
-    private String fechaInicio;
-    private String fechaFin;
-    private int totalDias;
-    private float valorTotal;
-    private boolean activo;
-
-    public ContratoRenting() {
-
-    }
-
-    public ContratoRenting(String idContrato, String cedulaCliente,
-            String placaVehiculo, String fechaInicio,
-            String fechaFin, int totalDias,
-            float valorTotal, boolean activo) {
-
-        this.idContrato = idContrato;
-        this.cedulaCliente = cedulaCliente;
-        this.placaVehiculo = placaVehiculo;
-        this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
-        this.totalDias = totalDias;
-        this.valorTotal = valorTotal;
         this.activo = activo;
     }
 
@@ -66,44 +44,7 @@ public class ContratoRenting {
         return fechaFin;
     }
 
-    public void setFechaFin(String fechaFin) {
-        this.fechaFin = fechaFin;
-    }
-
-    public int getTotalDias() {
-        return totalDias;
-    }
-
     public void setTotalDias(int totalDias) {
-        this.totalDias = totalDias;
-    }
-
-    public float getValorTotal() {
-        return valorTotal;
-    }
-
-    public void setValorTotal(float valorTotal) {
-        this.valorTotal = valorTotal;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
-
-    @Override
-    public String toString() {
-
-        return "\nID Contrato: " + idContrato
-                + "\nCedula Cliente: " + cedulaCliente
-                + "\nPlaca Vehiculo: " + placaVehiculo
-                + "\nFecha Inicio: " + fechaInicio
-                + "\nFecha Fin: " + fechaFin
-                + "\nTotal Dias: " + totalDias
-                + "\nValor Total: " + valorTotal
                 + "\nEstado: "
                 + (activo ? "Activo" : "Finalizado");
     }
